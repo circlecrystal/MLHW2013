fa04e7c324b6f9a3a3c55f08863c49df7b3e0127
circlecrystal <circlecrystal@hotmail.com>
2013-10-12 21:27:05 +0800

    Finished refactoring for the first time

--- Homework 1/PLA.py
+++ Homework 1/PLA.py
@@ -1,7 +1,9 @@
+from random import seed, shuffle
+
 def update_or_pass(w, x, y, longn=1):
     """x is (input space dimension + 1) length input data vector,
-       w is same victor length with x. y is a single number result.
-       return -1 if no update
+    w is same victor length with x. y is a single number result.
+    Return -1 if no update.
     """
     s = 0 # temporary sum
     temp = range(len(w))
@@ -12,15 +14,18 @@ def update_or_pass(w, x, y, longn=1):
     else:
         return list(w[j] + longn*y*x[j] for j in temp)
 
+
 def getd(path):
     d = len(open(path).readline().split())
     return d
 
+
 def getn(path):
     fin_preload = open(path)
-    n = sum(1 for line in fin_preload) # number of data
+    n = sum(1 for line in fin_preload)  # number of data
     fin_preload.close()
     return n
+
 
 def xylist(path):
     d = getd(path)
@@ -36,21 +41,24 @@ def xylist(path):
     fin.close()
     return (listx, listy)
 
+
 def readxy(i, listxy):
     (listx, listy) = listxy
     return (listx[i], listy[i])
 
-def nextxy(i, listxy):
-    return readxy(i, listxy)
 
-def nextxy_readlist(readlist, loop, n, listxy):
-    return readxy(readlist[loop % n], listxy)
+def nextxy(i, listxy, readlist=None):
+    if readlist == None:
+        return readxy(i, listxy)
+    else:
+        return readxy(readlist[i], listxy)
+
 
 def nextxy_pure_random(randomlist, listxy):
-    from random import seed, shuffle
     seed()
     shuffle(randomlist)
     return readxy(randomlist[-1], listxy)
+
 
 def errorset(w, listxy):
     n = len(listxy[1])
@@ -62,17 +70,18 @@ def errorset(w, listxy):
             errorlist.append(i)
     return errorlist
 
-def pla_core(path):
+
+def pla_core(path, longn=1, readlist=None):
     n = getn(path)
     d = getd(path)
     listxy = xylist(path)
-    w = [0] * d # w0
-    count = 0 # number of updates
-    s = 0 # number of continuous passes
-    loop = 0 # number of loops
+    w = [0] * d  # w0
+    count = 0  # number of updates
+    s = 0  # number of continuous passes
+    loop = 0  # number of loops
     while True:
-        (x, y) = nextxy(loop % n, listxy)
-        t = update_or_pass(w, x, y)
+        (x, y) = nextxy(loop % n, listxy, readlist)
+        t = update_or_pass(w, x, y, longn)
         loop += 1
         if t == -1:
             s += 1
@@ -84,34 +93,13 @@ def pla_core(path):
             w = t
     return (w, count)
 
-def pla_readlist_core(path, readlist, longn=1):
-    n = getn(path)
-    d = getd(path)
-    listxy = xylist(path)
-    w = [0] * d # w0
-    count = 0 # number of updates
-    s = 0 # number of continuous passes
-    loop = 0 # number of loops
-    while True:
-        (x, y) = nextxy_readlist(readlist, loop, n, listxy)
-        t = update_or_pass(w, x, y, longn)
-        loop += 1
-        if t == -1:
-            s += 1
-            if s == n:
-                break
-        else:
-            s = 0
-            count += 1
-            w = t
-    return(w, count)
 
 def pla_random_core(path, pla_updates):
     n = getn(path)
     d = getd(path)
     listxy = xylist(path)
-    w = [0] * d # w0
-    count = 0 # number of updates
+    w = [0] * d
+    count = 0
     w_pocket = w
     errornumber_pocket = n
     while count < pla_updates:
@@ -127,24 +115,27 @@ def pla_random_core(path, pla_updates):
         count += 1
     return (w_pocket, count)
 
+
 def pla_fixed_sequential(path):
     (w, count) = pla_core(path)
     return count
 
+
 def pla_fixed_random(path, times=2000, longn=0.5):
-    from random import seed, shuffle
     n = getn(path)
     readlist = list(range(n))
     sumofcounts = 0
     for i in range(times):
         seed()
         shuffle(readlist)
-        (w, count) = pla_readlist_core(path, readlist, longn)
+        (w, count) = pla_core(path, longn, readlist)
         sumofcounts += count
     return sumofcounts / times
 
+
 def pla_pure_random(path, pla_updates=50):
     return pla_random_core(path, pla_updates)
+
 
 def test(listxy, w):
     n = len(listxy[1])
@@ -155,6 +146,7 @@ def test(listxy, w):
             error_count += 1
     return error_count / n
 
+
 def verify(trainpath, testpath, times):
     listxy = xylist(testpath)
     sum_of_error_rate = 0
@@ -162,6 +154,7 @@ def verify(trainpath, testpath, times):
         (w_pocket, temp) = pla_pure_random(trainpath)
         sum_of_error_rate += test(listxy, w_pocket)
     return sum_of_error_rate / times
+
 
 traindata = 'hw1_18_train.dat'
 testdata = 'hw1_18_test.dat'
